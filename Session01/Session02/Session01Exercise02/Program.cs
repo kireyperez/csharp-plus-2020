d539294b7e997d43028cce98abe69ccdc575b6c5
KIRESAURUS\Kire <kirey.perez@studerande.plushogskolan.se>
2020-08-26 11:54:27 +0200

    Session02Exercise02

---
+++ Session01/Session02/Session01Exercise02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Session01Exercise02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Do you want to enter your name? (y/n)? ");
+            var key = Console.ReadKey();
+            if (key.KeyChar == 'n')
+                return;
+            Console.WriteLine("Enter your name:");
+            var name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name);
+            Console.ReadKey();
+        }
+    }
+}
