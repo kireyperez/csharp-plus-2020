727bf5c4cc5dd26da1e2c4b200a37f15118105e4
KIRESAURUS\Kire <kirey.perez@studerande.plushogskolan.se>
2020-08-26 10:17:17 +0200

    Initial commit -Session02

---
+++ Session01/Session02/Session02Exercise01/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Session02Exercise01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+    }
+}
